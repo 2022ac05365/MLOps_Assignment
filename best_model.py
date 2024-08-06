cd88ae030b799f447b917b317621cdf81e290674
Yelem Sreenivasalu <2022ac05406@wilp.bits-pilani.ac.in>
2024-08-06 00:57:04 -0500

    fix the bug

--- best_model.py
+++ best_model.py
@@ -1,7 +1,13 @@
 import joblib # type: ignore
 import optuna # type: ignore
+from sklearn.datasets import load_iris # type: ignore
 from sklearn.ensemble import RandomForestClassifier # type: ignore
 from sklearn.model_selection import train_test_split # type: ignore
+
+# Load the dataset
+iris = load_iris()
+X = iris.data
+y = iris.target
 
 # Split the dataset
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
